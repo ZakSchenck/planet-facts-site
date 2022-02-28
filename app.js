4ffad3dcb83956e19af55673499abbd4ed7e245a
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-02-28 08:54:59 -0500

    Finished javascript for planet images

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const planetBtn = Array.from(document.querySelectorAll('.planet-btn'));
 const overviewBtn = Array.from(document.querySelectorAll('.overview-btn'));
-let description = document.querySelector('.description');
+const description = document.querySelector('.description');
+const source = document.querySelector('.source');
+const nav = document.querySelector('.nav');
+const planetImage = document.querySelector('.planet-img')
 let planetIndex = 0
 
 fetch('data.json').then(function (response) {
@@ -8,25 +11,32 @@ fetch('data.json').then(function (response) {
 }).then(function (data) {
     console.log(data);
     description.innerText = data[0].overview.content;
+    source.innerText = data[0].overview.source;
+    planetImage.src = data[0].images.planet
 
     planetBtn.forEach(button => {
         button.addEventListener('click', (e) => {
             planetIndex = planetBtn.indexOf(e.target);
             button[planetIndex] = description.innerText = data[planetIndex].overview.content;
+            source.innerText = data[planetIndex].overview.source;
+            planetImage.src = data[planetIndex].images.planet
         })
     })
 
-    overviewBtn.forEach(btn => {
-        btn.addEventListener('click', (event) => {
+    overviewBtn.forEach(button => {
+        button.addEventListener('click', (event) => {
             let overviewIndex = overviewBtn.indexOf(event.target);
             if (overviewIndex === 0) {
                 description.innerText = data[planetIndex].overview.content;
+                planetImage.src = data[planetIndex].images.planet
             }
             if (overviewIndex === 1) {
                 description.innerText = data[planetIndex].structure.content;
+                planetImage.src = data[planetIndex].images.internal
             }
             if (overviewIndex === 2) {
                 description.innerText = data[planetIndex].geology.content;
+                planetImage.src = data[planetIndex].images.geology
             }
         })
     })
